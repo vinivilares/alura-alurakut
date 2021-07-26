09fbd0e73edfa12c4ce80804a27578aae27a9c2f
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2021-07-25 21:02:35 -0300

    criação de banco de dados com api datocms

--- pages/index.js
+++ pages/index.js
@@ -51,23 +51,7 @@ function ProfileRelationsBox(propriedades) {
 
 export default function Home() {
   const githubUser = 'vinivilares';
-  const [comunidades, setComunidades] = React.useState([{
-    id: '23192738912739812312',
-    title: 'Eu Odeio Acordar Cedo',
-    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
-  }, {
-    id: '2873891273981298312',
-    title: 'Queria Sorvete Mas Era Feijão',
-    image: 'https://scontent.fcgh4-1.fna.fbcdn.net/v/t1.6435-9/167215787_4114938901863525_9196317370430945342_n.png?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeHIA9ASpRd7ecoip8ciyht21KVSLnCPtlrUpVIucI-2Wuq9tyfrZAvGyvXmDelGo3hTP223jDkbPlVRmya-1i6f&_nc_ohc=r3YCuniUOswAX-Es7tX&_nc_ht=scontent.fcgh4-1.fna&oh=d86430bab14ad21d5919cc02e09da948&oe=60FB1E92'
-  }, {
-    id: '23124515611238312',
-    title: 'Buddy Poke',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuusEVNegm_nddYnmqlFobFB5JG3ynZg-26-Mo9pxff0FES35JOu4TxbrQzFTrJQmPs2E&usqp=CAU'
-  }, {
-    id: '2312231456685238312',
-    title: 'Eu Odeio Esperar Resposta no MSN',
-    image: 'https://img10.orkut.br.com/community/9197344aa3f8d682ece581e1146bec4a.jpg'
-  }]);
+  const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -84,6 +68,30 @@ export default function Home() {
       return respostaDoServidor.json();
     }).then(function (respostaCompleta) {
       setSeguidores(respostaCompleta);
+    })
+
+    //API Graph
+    fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'f40ca1b1b30bc6885228cf36731374',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ "query": `query {
+        allCommunities {
+          id
+          title
+          imageUrl
+          creatorSlug
+        }
+      }` })
+    })
+    .then((response) => response.json()) // Pega o retorno do response.json() e já retorna
+    .then((respostaCompleta) => {
+      const comunidadesVindasDoDato = respostaCompleta.data.allCommunities;
+      setComunidades(comunidadesVindasDoDato)
+      // console.log(comunidades)
     })
   }, [])
 
@@ -107,16 +115,28 @@ export default function Home() {
             <h2 className="subTitle">O que você deseja fazer?</h2>
             <form onSubmit={function handleCriaComunidade(e) {
               e.preventDefault();
+              
               const dadosDoForm = new FormData(e.target);
+              
               const comunidade = {
-                id: new Date().toISOString(),
                 title: dadosDoForm.get('title'),
-                image: dadosDoForm.get('image')
+                imageUrl: dadosDoForm.get('image'),
+                creatorSlug: githubUser,
               }
 
-              // comunidades.push('Alura Stars');
-              const comunidadesAtualizadas = [...comunidades, comunidade];
-              setComunidades(comunidadesAtualizadas)
+              fetch('/api/comunidades', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(comunidade)
+              }).then(async (response) => {
+                const dados = await response.json();
+                // console.log(dados.registroCriado);
+                const comunidade = dados.registroCriado;
+                const comunidadesAtualizadas = [...comunidades, comunidade];
+                setComunidades(comunidadesAtualizadas)
+              })
             }}>
               <div>
                 <input placeholder="Qual vai ser o nome da sua comunidade?" name="title" aria-label="Qual vai ser o nome da sua comunidade?" type="text" />
@@ -157,8 +177,8 @@ export default function Home() {
               {comunidades.map((itemAtual) => {
                 return (
                   <li key={itemAtual.id}>
-                    <a href={`/users/${itemAtual.title}`}>
-                      <img src={itemAtual.image} />
+                    <a href={`/communities/${itemAtual.id}`}>
+                      <img src={itemAtual.imageUrl} />
                       <span>{itemAtual.title}</span>
                     </a>
                   </li>
